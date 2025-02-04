88218b2403186ab6b636e7b2abbf09b598cac413
GabrielKSB <gabrielkennedybrito@hotmail.com>
2025-02-04 15:31:30 -0300

    Update: Auth, return user without password

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,9 +2,11 @@ const { User } = require("../models");
 
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { password } = require("../config/database");
+const { SELECT } = require("sequelize/lib/query-types");
 //const JWT_SECRET = process.env.JWT_SECRET;
-const JWT_SECRET = "9e0a77e143feb7a51acdc810b1b839d4edd4847716d62a9269335723a8a5a6bd"
-
+const JWT_SECRET =
+  "9e0a77e143feb7a51acdc810b1b839d4edd4847716d62a9269335723a8a5a6bd";
 
 class UserController {
   async store(req, res) {
@@ -21,9 +23,8 @@ class UserController {
         return res.status(400).json({ message: "Insira um nome e email." });
       }
 
-   
       const hashPassword = await bcrypt.hash(password, 10);
-      console.log(hashPassword)
+      console.log(hashPassword);
 
       const createdUser = await User.create({
         name,
@@ -39,61 +40,100 @@ class UserController {
     }
   }
 
-  async index(req, res) {
+  async listAll(req, res) {
     try {
-      const users = await User.findAll();
-      return res.status(200).json(users);
+      const token = req.headers.authorization;
+
+      if (!token) {
+        return res.status(401).json({ message: "Acesso Negado" });
+      }
+
+      try {
+        const decoded = jwt.verify(token.replace("Bearer ", ""), JWT_SECRET);
+
+        req.userId = decoded.id;
+
+        const users = await User.findAll({
+          attributes: ['id', 'email', 'number', 'area_code', 'createdAt', 'updatedAt']
+        });
+
+        return res.status(200).json(users);
+      } catch (err) {
+        return res
+          .status(401)
+          .json({ message: "Token Inválido, Acesso não autorizado" });
+      }
     } catch (err) {
       return res.status(400).json({ message: "Falha ao cadastrar usuário" });
     }
   }
 
-  async show(req, res) {
+  async listOne(req, res) {
     try {
-      const { id } = req.params;
+      const token = req.headers.authorization;
 
-      const user = await User.findByPk(id);
-
-      if (!user) {
-        return res.status(404).json({ message: "Usuário não Encontrado" });
+      if (!token) {
+        return res.status(401).json({ message: "Acesso Negado" });
       }
 
-      return res.status(200).json(user);
+      try {
+        const decoded = jwt.verify(token.replace("Bearer ", ""), JWT_SECRET);
+
+        req.userId = decoded.id;
+
+        const { id } = req.params;
+
+        const user = await User.findByPk(id);
+
+        if (!user) {
+          return res.status(404).json({ message: "Usuário não Encontrado" });
+        }
+
+        return res.status(200).json(user);
+      } catch (err) {
+        return res
+          .status(401)
+          .json({ message: "Token Inválido, Acesso não autorizado" });
+      }
     } catch (err) {
       return res.status(400).json({ message: "Falha ao listar usuário" });
     }
   }
 
-  async login(req, res){
-    try{
-      const userInfo = req.body
+  async login(req, res) {
+    try {
+      const userInfo = req.body;
 
       //Buscar o úsuario no banco
       const user = await User.findOne({ where: { email: userInfo.email } });
 
       //verificar se o usuario existe no banco
-      if(!user){
-        return res.status(404).json({message: "usuário não encontrado"})
+      if (!user) {
+        return res.status(404).json({ message: "usuário não encontrado" });
       }
 
       //Comparar se as senhas do banco e a inserida pelo usuario são iguais
-      const isMatch = await bcrypt.compare(userInfo.password, user.password)
+      const isMatch = await bcrypt.compare(userInfo.password, user.password);
 
-      if(!isMatch){
-        return res.status(400).json({message: "E-mail ou Senha Inválida"})
+      if (!isMatch) {
+        return res.status(400).json({ message: "E-mail ou Senha Inválida" });
       }
 
       //Gerar o Token JWT
-      const token = jwt.sign({
-        id: user.id,
-        email: user.email
-      }, JWT_SECRET, {expiresIn: '1m'})
+      const token = jwt.sign(
+        {
+          id: user.id,
+          email: user.email,
+        },
+        JWT_SECRET,
+        { expiresIn: "2m" }
+      );
 
-      res.status(200).json(token)
-
-
-    }catch(err){
-      return res.status(500).json({message: "Erro no Servidor, tente novamente"})
+      res.status(200).json(token);
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ message: "Erro no Servidor, tente novamente" });
     }
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -8,9 +8,9 @@ routes.get("/health", (req, res) => {
 });
 
 
-routes.get('/users', UserController.index) //Listar usuarios
-routes.get('/users/:id', UserController.show) //Listar um único usuario de acordo com a ID
+routes.get('/users', UserController.listAll) //Listar usuarios
+routes.get('/users/:id', UserController.listOne) //Listar um único usuario de acordo com a ID
 routes.post('/users', UserController.store) //Registrar o Usuário
-routes.post('/users/login', UserController.login) //Realizar o Login
+routes.post('/users/login',UserController.login) //Realizar o Login
 
 module.exports = routes;
